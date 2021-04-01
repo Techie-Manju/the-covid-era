500e847fd5ef2249d38a473186747efddaa56a69
the-Baap <80066209+the-Baap@users.noreply.github.com>
2021-04-01 10:39:39 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Manju's Space`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Manju Bala`,
+      summary: `who lives and works in India building useful web applications.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
